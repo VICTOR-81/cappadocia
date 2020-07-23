fa706993e367490c1ed5db7713a3b69c6433f667
VICTOR-81 <v161181@gmail.com>
2020-07-23 12:10:43 +0700

    верстка

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(function(){
-
+$('.fairy-tail__slider').slick({
+  prevArrow: '<button type="button" class="slick-btn slick-prev">​<img src="images/arrow-prev.svg" alt=""></button>',
+  nextArrow: '<button type="button" class="slick-btn slick-next">​<img src="images/arrow-next.svg" alt=""></button>',
+  autoplay: true,
+  fade: true
+});
 });
