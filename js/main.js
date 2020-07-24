0dc5f77afa4a5e10616d3c80d3567f12e330ddb6
VICTOR-81 <v161181@gmail.com>
2020-07-24 16:59:47 +0700

    верстка

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function(){
-$('.fairy-tail__slider').slick({
+$('.fairy-tail__slider, .our-trip__slider').slick({
   prevArrow: '<button type="button" class="slick-btn slick-prev">​<img src="images/arrow-prev.svg" alt=""></button>',
   nextArrow: '<button type="button" class="slick-btn slick-next">​<img src="images/arrow-next.svg" alt=""></button>',
   autoplay: true,
