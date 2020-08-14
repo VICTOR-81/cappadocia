50a30a85cc2b8213da6ad96f7372512fb20aa6be
VICTOR-81 <v161181@gmail.com>
2020-08-14 20:08:37 +0700

    верстка

--- js/main.js
+++ js/main.js
@@ -3,6 +3,22 @@ $('.fairy-tail__slider, .our-trip__slider').slick({
   prevArrow: '<button type="button" class="slick-btn slick-prev">​<img src="images/arrow-prev.svg" alt=""></button>',
   nextArrow: '<button type="button" class="slick-btn slick-next">​<img src="images/arrow-next.svg" alt=""></button>',
   autoplay: true,
-  fade: true
+  fade: true,
+  responsive:[{ breakpoint: 601, settings: { arrows: false } },]
 });
+
+$(".menu").on("click","a", function (event) {
+  //отменяем стандартную обработку нажатия по ссылке
+  event.preventDefault();
+
+  //забираем идентификатор бока с атрибута href
+  var id  = $(this).attr('href'),
+
+  //узнаем высоту от начала страницы до блока на который ссылается якорь
+    top = $(id).offset().top;
+  
+  //анимируем переход на расстояние - top за 1500 мс
+  $('body,html').animate({scrollTop: top}, 1500);
+});
+
 });
